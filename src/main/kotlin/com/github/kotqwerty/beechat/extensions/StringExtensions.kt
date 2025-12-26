80f43530d629e6a4280041b63d669661383557e1
KotQWERTY <151190068+KotQWERTY@users.noreply.github.com>
2025-12-26 14:45:46 +0100

    feat: remove StringExtensions.kt

--- src/main/kotlin/com/github/kotqwerty/beechat/extensions/StringExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.github.kotqwerty.beechat.extensions
-
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
-
-fun String.miniMessage(tagResolver: TagResolver = TagResolver.empty()) =
-    MiniMessage.miniMessage().deserialize(this, tagResolver)
