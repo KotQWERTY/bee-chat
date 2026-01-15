f872d2555935dc691552bdec80b9e8ecfd0da110
KotQWERTY <151190068+KotQWERTY@users.noreply.github.com>
2026-01-15 11:59:31 +0100

    feat: create Messages.kt

---
+++ src/main/kotlin/com/github/kotqwerty/beechat/configuration/Messages.kt
@@ -0,0 +1,11 @@
+package com.github.kotqwerty.beechat.configuration
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+
+@ConfigSerializable
+data class Messages(
+    val reload: String = "<green>The <gradient:yellow:gold>BeeChat</gradient> configuration has been reloaded",
+    val notPlayer: String = "<red>This command can only be executed by a player",
+    val spyModeEnabled: String = "<aqua>Spy</aqua> mode <green>enabled</green>. Enter the command again to <red>disable</red>",
+    val spyModeDisabled: String = "<aqua>Spy</aqua> mode <red>disabled</red>",
+)
