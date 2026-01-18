cdf16ef229737d0b71dfcf1de161cbc9c8f8be2b
KotQWERTY <151190068+KotQWERTY@users.noreply.github.com>
2026-01-18 16:03:55 +0100

    feat: add `CHAT_CHANNEL` to Keys

--- src/main/kotlin/com/github/kotqwerty/beechat/Keys.kt
+++ src/main/kotlin/com/github/kotqwerty/beechat/Keys.kt
@@ -4,4 +4,5 @@ import org.bukkit.NamespacedKey
 
 object Keys {
     val SPY_MODE = NamespacedKey(BeeChat.NAMESPACE, "spy_mode")
+    val CHAT_CHANNEL = NamespacedKey(BeeChat.NAMESPACE, "chat_channel")
 }
