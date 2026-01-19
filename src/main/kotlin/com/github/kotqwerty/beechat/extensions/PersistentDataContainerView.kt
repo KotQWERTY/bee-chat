95da1a7740926becbd1eceb6e19c1e34f722d7f5
KotQWERTY <151190068+KotQWERTY@users.noreply.github.com>
2026-01-19 18:20:51 +0100

    feat: add `PersistentDataContainerView.getBoolOrNull` and `getBool` extension functions

--- src/main/kotlin/com/github/kotqwerty/beechat/extensions/PersistentDataContainerView.kt
+++ src/main/kotlin/com/github/kotqwerty/beechat/extensions/PersistentDataContainerView.kt
@@ -6,3 +6,9 @@ import org.bukkit.persistence.PersistentDataType
 
 fun PersistentDataContainerView.getStringOrNull(key: NamespacedKey): String? =
     get(key, PersistentDataType.STRING)
+
+fun PersistentDataContainerView.getBoolOrNull(key: NamespacedKey): Boolean? =
+    get(key, PersistentDataType.BOOLEAN)
+
+fun PersistentDataContainerView.getBool(key: NamespacedKey, default: Boolean = false) =
+    getBoolOrNull(key) ?: default
